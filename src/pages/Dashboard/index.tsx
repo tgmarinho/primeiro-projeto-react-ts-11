5403a662c18c90eda8f6ec94e5af9ee6d8155a97
Thiago Marinho <tgmarinho@gmail.com>
2020-04-20 17:37:32 -0400

    React - Criando a aplicação - Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,96 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repostórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit"> Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
+            alt="Thiago"
+          />
+          <div>
+            <strong>tgmarinho/bootcamp</strong>
+            <p>desciprtion of project on github</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/pages/Dashboard/styles.tsx
+++ src/pages/Dashboard/styles.tsx
@@ -1,3 +1,96 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  max-width: 450px;
+  line-height: 56px;
+  margin-top: 80px;
+`;
+
+export const Form = styled.form`
+  margin-top: 40px;
+  max-width: 700px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 70px;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &::placeholder {
+      color: #a8a8b3;
+    }
+  }
+
+  button {
+    width: 210px;
+    height: 70px;
+    color: #fff;
+    font-weight: bold;
+    border: 0;
+    background: #04d361;
+    border-radius: 0px 5px 5px 0px;
+    transition: background-color 0.4s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
+  }
+`;
+
+export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
+
+  a {
+    background: #fff;
+    border-radius: 5px;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    &:hover {
+      transform: translateX(10px);
+    }
+  }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin-left: 16px;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+      p {
+        margin-top: 4px;
+        font-size: 18px;
+        color: #a8a8b3;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+
+
+  }
+`;
