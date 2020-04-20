750db2031668bb0316abfb6ebfcb801fd2f9c192
Thiago Marinho <tgmarinho@gmail.com>
2020-04-20 16:22:11 -0400

    React - Criando a aplicação - utilizando styled componenets

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
 );
 
 export default App;
