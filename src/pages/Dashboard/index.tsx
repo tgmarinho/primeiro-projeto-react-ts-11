e2979d2d9a3c120342c1c903dfd634f05659543f
Thiago Marinho <tgmarinho@gmail.com>
2020-04-21 09:49:22 -0400

    React - Criando a aplicação - Conectando a API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,93 +1,64 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
+
+interface Repository {
+  full_name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+  description: string;
+}
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repostórios no Github</Title>
 
-      <Form action="">
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          placeholder="Digite o nome do repositório"
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit"> Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/380327?s=400&u=61b426b901b8fe02e12019b1fdb67bf0072d4f00&v=4"
-            alt="Thiago"
-          />
-          <div>
-            <strong>tgmarinho/bootcamp</strong>
-            <p>desciprtion of project on github</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
